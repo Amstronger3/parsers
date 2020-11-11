371fb6fe27fb77b8aa00cbe0e18b18e1b68a36e9
Amstronger3 <61287091+Amstronger3@users.noreply.github.com>
2020-11-12 01:02:24 +0200

    add link scraper

--- src/async_parser.py
+++ src/async_parser.py
@@ -15,15 +15,34 @@
 # results = loop.run_until_complete(asyncio.gather(*tasks))
 # print("Results: %s" % results)
 
+import asyncio
 import os
 import pathlib
-import aiohttp
-import aiofiles
-import asyncio
-from src.utils import read_links_txt, get_file_name_from_url
 
-links = read_links_txt('../input/images_links.txt')
-img_dir = '../img/async'
+import aiofiles
+import aiohttp
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from src.async_parser import img_dir
+from src.utils import get_file_name_from_url
+
+driver = webdriver.Firefox()
+url_link = "https://pixabay.com/"
+
+
+def find_links(url_link):
+    driver.get(url_link)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    clear_links = list()
+    links = (elem.get('content') for elem in soup.find_all('meta'))
+    for i in links:
+        if i is not None:
+            if i.startswith('https') and i.endswith('.jpg') or i.endswith('.png'):
+                clear_links.append(i)
+    return clear_links
+
 
 pathlib.Path(img_dir).mkdir(parents=True, exist_ok=True)
 
@@ -35,6 +54,8 @@ async def download(url):
             await f.write(await response.read())
             await f.close()
 
+
+line = find_links(url_link)
 loop = asyncio.get_event_loop()
-tasks = [download(link) for link in links]
+tasks = [download(link) for link in line]
 loop.run_until_complete(asyncio.gather(*tasks))
